fdfd5d5fdc81dc91c8239ae9b59585e680a26497
sergeibux <sertux.dev@gmail.com>
2021-05-25 17:40:46 +0200

    new recipe : more ingredients

--- list2courses/lib/recipes.dart
+++ list2courses/lib/recipes.dart
@@ -24,10 +24,81 @@ class Recipes extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        // onPressed: _incrementCounter,
+        onPressed: () => _newRecipe(context),
         tooltip: 'Nouvelle recette',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+
+  Future<void> _newRecipe(BuildContext context) async {
+    String _recipeName;
+    int _ingredientsCount = 0;
+    List<String> _ingredients = new List<String>();
+    List<TextField> _ingredientsWidgets = new List<TextField>();
+
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (_) {
+          return MyDialog();
+        });
+  }
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _addIngrWidget createState() => new _addIngrWidget();
+}
+
+class _addIngrWidget extends State<MyDialog> {
+  List<TextField> _ingredientsWidgets = new List<TextField>();
+  int _ingredientsCount = 0;
+  String _recipeName;
+
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Nouvelle recette'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text('Nom'),
+            TextField(onChanged: (value) {
+              _recipeName = value;
+            }),
+            Text('Liste des ingrédients'),
+            ListBody(
+              children: _ingredientsWidgets,
+            ),
+            Text('Ajouter un ingrédient'),
+            ElevatedButton(
+              child: Icon(Icons.add_box_rounded),
+              onPressed: () => setState(
+                () {
+                  ++_ingredientsCount;
+                  _ingredientsWidgets = _addIngr(context, _ingredientsWidgets);
+                  for (int i = 0; i < _ingredientsCount; ++i){
+                    child : _ingredientsWidgets[i];
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Ajouter'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  List<TextField> _addIngr(BuildContext context, List<TextField> _ingredientsWidgets) {
+    _ingredientsWidgets.add(new TextField());
+    return _ingredientsWidgets;
   }
 }
