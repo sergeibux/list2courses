33e4da692c490498c59ea06a695b58fcc9a66347
NinonTz <ninon.touzeau@gmail.com>
2021-05-26 12:05:58 +0200

    ajout de la liste de courses

--- list2courses/lib/shoppingLists.dart
+++ list2courses/lib/shoppingLists.dart
@@ -30,4 +30,63 @@ class ShoppingLists extends StatelessWidget {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<void> _newShoppingList(BuildContext context) async {
+    String _shoppingListName;
+    int _productsCount = 0;
+    List<String> _products = new List<String>();
+    List<TextField> _productsWidgets = new List<TextField>();
+
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (_) {
+          return MyDialog();
+        });
+  }
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _addProductsWidget createState() => new _addProductsWidget();
+}
+
+class _addProductsWidget extends State<MyDialog> {
+  List<TextField> _productsWidgets = new List<TextField>();
+  int _productsCount = 0;
+  String _productName;
+  String _productUnit;
+  int _productQuantity;
+
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Nouvelle liste de courses'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text('Nom'),
+            TextField(onChanged: (value) {
+              _productName = value;
+            }),
+            Text('Quantité'),
+            TextField(onChanged: (value) {
+              _productQuantity = value as int;
+            }, keyboardType: TextInputType.number,),
+            Text('Unité'),
+            TextField(onChanged: (value) {
+              _productUnit = value;
+            }),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Ajouter'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
 }
