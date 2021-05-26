d467aba4169c150faad7249d41463c203dd3eda3
sergeibux <sertux.dev@gmail.com>
2021-05-26 10:57:11 +0200

    new recipe : ingredient : unit and quantities

--- list2courses/lib/recipes.dart
+++ list2courses/lib/recipes.dart
@@ -52,11 +52,20 @@ class MyDialog extends StatefulWidget {
 }
 
 class _addIngrWidget extends State<MyDialog> {
-  List<TextField> _ingredientsWidgets = new List<TextField>();
+  TextField _ingredientsWidgets = new TextField();
+  TextField _qttsWidgets = new TextField();
+  TextField _unitsWidgets = new TextField();
+  List<Row> _ingrRows = new List<Row>();
   int _ingredientsCount = 0;
   String _recipeName;
 
   Widget build(BuildContext context) {
+    SizedBox sbQtt =
+        new SizedBox.fromSize(child: new Text("Quantité"), size: Size(70, 40));
+    SizedBox sbUnit =
+        new SizedBox.fromSize(child: new Text("Unité"), size: Size(45, 40));
+    SizedBox sbIngr = new SizedBox.fromSize(
+        child: new Text("Ingrédients"), size: Size(90, 40));
     return AlertDialog(
       title: const Text('Nouvelle recette'),
       content: SingleChildScrollView(
@@ -67,18 +76,32 @@ class _addIngrWidget extends State<MyDialog> {
               _recipeName = value;
             }),
             Text('Liste des ingrédients'),
-            ListBody(
-              children: _ingredientsWidgets,
-            ),
-            Text('Ajouter un ingrédient', style : TextStyle(color: Colors.blueAccent)),
+            Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent, width: 1)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [sbQtt, sbUnit, sbIngr],
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly
+                    ),
+                    ListBody(
+                      children: _ingrRows,
+                    ),
+                  ],
+                )),
+            Text('Ajouter un ingrédient',
+                style: TextStyle(color: Colors.blueAccent)),
             ElevatedButton(
               child: Icon(Icons.add_box_rounded),
               onPressed: () => setState(
                 () {
                   ++_ingredientsCount;
-                  _ingredientsWidgets = _addIngr(context, _ingredientsWidgets);
-                  for (int i = 0; i < _ingredientsCount; ++i){
-                    child : _ingredientsWidgets[i];
+                  _addIngr(context);
+                  for (int i = 0; i < _ingredientsCount; ++i) {
+                    child:
+                    _ingrRows[i];
                   }
                 },
               ),
@@ -97,8 +120,22 @@ class _addIngrWidget extends State<MyDialog> {
     );
   }
 
-  List<TextField> _addIngr(BuildContext context, List<TextField> _ingredientsWidgets) {
-    _ingredientsWidgets.add(new TextField());
-    return _ingredientsWidgets;
+  void _addIngr(BuildContext context) {
+    _qttsWidgets = new TextField(keyboardType: TextInputType.number);
+    _unitsWidgets = new TextField();
+    _ingredientsWidgets = new TextField();
+    List<SizedBox> l = new List<SizedBox>();
+    SizedBox sbQtt =
+        new SizedBox.fromSize(child: _qttsWidgets, size: Size(70, 40));
+    SizedBox sbUnit =
+        new SizedBox.fromSize(child: _unitsWidgets, size: Size(45, 40));
+    SizedBox sbIngr =
+        new SizedBox.fromSize(child: _ingredientsWidgets, size: Size(90, 40));
+    l.add(sbQtt);
+    l.add(sbUnit);
+    l.add(sbIngr);
+    Row r =
+        new Row(children: l, mainAxisAlignment: MainAxisAlignment.spaceEvenly);
+    _ingrRows.add(r);
   }
 }
